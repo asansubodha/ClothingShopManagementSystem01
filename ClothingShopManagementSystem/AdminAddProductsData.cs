797e7b7e5e3f4fdd6c5f2c9eb435b9aefdc7c55b
R.M Asan Subodha Madusankha <ashansubodha8@gmail.com>
2024-12-01 22:19:07 +0530

    Add project files.

---
+++ ClothingShopManagementSystem/AdminAddProductsData.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClothingShopManagementSystem
+{
+    class AdminAddProductsData
+    {
+         public int ID { set; get; } // 0
+         public string ProductID { set; get; } // 1
+        public string ProductName { set; get; } // 2
+        public string Type { set; get; } // 3
+        public string Stock { set; get; } // 4
+        public string Price { set; get; } // 5
+        public string Status { set; get; } // 6
+        public string Image { set; get; } // 7
+        public string DateInsert { set; get; } // 8
+        public string DateUpdate { set; get; } // 9
+
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\hp\OneDrive\Documents\CShop.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=False");
+    
+        public List<AdminAddProductsData> productsListData()
+        {
+            List<AdminAddProductsData> listData = new List<AdminAddProductsData>();
+
+            if(connect.State == ConnectionState.Closed)
+            {
+                try
+                {
+                    connect.Open();
+
+                    string selectData = "SELECT * FROM products WHERE date_delete IS NULL";
+
+                    using(SqlCommand cmd = new SqlCommand(selectData, connect))
+                    {
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            AdminAddProductsData apd = new AdminAddProductsData();
+
+                            apd.ID = (int)reader["Id"];
+                            apd.ProductID = reader["ProductID"].ToString();
+                            apd.ProductName = reader["ProductName"].ToString();
+                            apd.Type = reader["Type"].ToString();
+                            apd.Stock = reader["Stock"].ToString();
+                            apd.Price = reader["Price"].ToString();
+                            apd.Status = reader["Status"].ToString();
+                            apd.Image = reader["Image"].ToString();
+                            apd.DateInsert = reader["DateInsert"].ToString();
+                            apd.DateUpdate = reader["DateUpdate"].ToString();
+
+                            Console.WriteLine($"Product: {apd.ProductName}, Status: {apd.Status}");
+
+                            listData.Add(apd);
+                        }
+
+                        //reader.Close();
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Failed connection: " + ex);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+
+            return listData;
+        }
+
+        public List<AdminAddProductsData> availableProductsData()
+        {
+            List<AdminAddProductsData> listData = new List<AdminAddProductsData>();
+
+            if(connect.State == ConnectionState.Closed)
+            {
+                try
+                {
+                    connect.Open();
+
+                    string selectData = "SELECT * FROM products WHERE Status = @stats";
+
+                    using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@stats", "Available");
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            AdminAddProductsData apd = new AdminAddProductsData();
+
+                            apd.ID = (int)reader["Id"];
+                            apd.ProductID = reader["ProductID"].ToString();
+                            apd.ProductName = reader["ProductName"].ToString();
+                            apd.Type = reader["Type"].ToString();
+                            apd.Stock = reader["Stock"].ToString();
+                            apd.Price = reader["Price"].ToString();
+
+                            listData.Add(apd);
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Failed Connection: " + ex);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+            return listData;
+        }
+
+    }
+}
---
+++ ClothingShopManagementSystem/AdminDashboardForm.Designer.cs
@@ -0,0 +1,336 @@
+﻿
+namespace ClothingShopManagementSystem
+{
+    partial class AdminDashboardForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.dashboard_TIn = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.dashboard_TCust = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.dashboard_TI = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dashboard_TC = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(20, 30);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1629, 294);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(132)))), ((int)(((byte)(219)))));
+            this.panel5.Controls.Add(this.dashboard_TIn);
+            this.panel5.Controls.Add(this.pictureBox4);
+            this.panel5.Controls.Add(this.label8);
+            this.panel5.Location = new System.Drawing.Point(1240, 37);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(379, 212);
+            this.panel5.TabIndex = 3;
+            // 
+            // dashboard_TIn
+            // 
+            this.dashboard_TIn.AutoSize = true;
+            this.dashboard_TIn.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboard_TIn.ForeColor = System.Drawing.Color.White;
+            this.dashboard_TIn.Location = new System.Drawing.Point(184, 159);
+            this.dashboard_TIn.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.dashboard_TIn.Name = "dashboard_TIn";
+            this.dashboard_TIn.Size = new System.Drawing.Size(70, 32);
+            this.dashboard_TIn.TabIndex = 11;
+            this.dashboard_TIn.Text = "$0.0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(185, 43);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(128, 22);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Total Income";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(132)))), ((int)(((byte)(219)))));
+            this.panel4.Controls.Add(this.dashboard_TCust);
+            this.panel4.Controls.Add(this.pictureBox2);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(431, 41);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(379, 212);
+            this.panel4.TabIndex = 1;
+            // 
+            // dashboard_TCust
+            // 
+            this.dashboard_TCust.AutoSize = true;
+            this.dashboard_TCust.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboard_TCust.ForeColor = System.Drawing.Color.White;
+            this.dashboard_TCust.Location = new System.Drawing.Point(325, 155);
+            this.dashboard_TCust.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.dashboard_TCust.Name = "dashboard_TCust";
+            this.dashboard_TCust.Size = new System.Drawing.Size(30, 32);
+            this.dashboard_TCust.TabIndex = 5;
+            this.dashboard_TCust.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(180, 39);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(182, 22);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Total of Customers";
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(132)))), ((int)(((byte)(219)))));
+            this.panel6.Controls.Add(this.dashboard_TI);
+            this.panel6.Controls.Add(this.pictureBox3);
+            this.panel6.Controls.Add(this.label6);
+            this.panel6.Location = new System.Drawing.Point(835, 37);
+            this.panel6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(379, 212);
+            this.panel6.TabIndex = 2;
+            // 
+            // dashboard_TI
+            // 
+            this.dashboard_TI.AutoSize = true;
+            this.dashboard_TI.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboard_TI.ForeColor = System.Drawing.Color.White;
+            this.dashboard_TI.Location = new System.Drawing.Point(173, 159);
+            this.dashboard_TI.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.dashboard_TI.Name = "dashboard_TI";
+            this.dashboard_TI.Size = new System.Drawing.Size(70, 32);
+            this.dashboard_TI.TabIndex = 8;
+            this.dashboard_TI.Text = "$0.0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(175, 43);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(153, 22);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Today\'s Income";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(132)))), ((int)(((byte)(219)))));
+            this.panel3.Controls.Add(this.dashboard_TC);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Location = new System.Drawing.Point(27, 41);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(379, 212);
+            this.panel3.TabIndex = 0;
+            // 
+            // dashboard_TC
+            // 
+            this.dashboard_TC.AutoSize = true;
+            this.dashboard_TC.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboard_TC.ForeColor = System.Drawing.Color.White;
+            this.dashboard_TC.Location = new System.Drawing.Point(328, 155);
+            this.dashboard_TC.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.dashboard_TC.Name = "dashboard_TC";
+            this.dashboard_TC.Size = new System.Drawing.Size(30, 32);
+            this.dashboard_TC.TabIndex = 2;
+            this.dashboard_TC.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(183, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Total of Cashiers";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.pictureBox5);
+            this.panel2.Location = new System.Drawing.Point(20, 348);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1629, 544);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::ClothingShopManagementSystem.Properties.Resources.image;
+            this.pictureBox5.Location = new System.Drawing.Point(27, 4);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(1572, 536);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 0;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::ClothingShopManagementSystem.Properties.Resources.income;
+            this.pictureBox4.Location = new System.Drawing.Point(24, 43);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 9;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ClothingShopManagementSystem.Properties.Resources.customer;
+            this.pictureBox2.Location = new System.Drawing.Point(23, 39);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ClothingShopManagementSystem.Properties.Resources.cash;
+            this.pictureBox3.Location = new System.Drawing.Point(21, 43);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 6;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ClothingShopManagementSystem.Properties.Resources.user;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 39);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // AdminDashboardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "AdminDashboardForm";
+            this.Size = new System.Drawing.Size(1668, 917);
+            this.panel1.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label dashboard_TC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label dashboard_TCust;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label dashboard_TI;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label dashboard_TIn;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
